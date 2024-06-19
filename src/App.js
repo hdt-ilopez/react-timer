0e3e63b2679631617ec19776719bf6d504be7a51
hdt-ilopez <150853590+hdt-ilopez@users.noreply.github.com>
2024-06-19 10:18:42 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [timeStart, setTimeStart] = useState(false);
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    if (isActive) {
+      intervalRef.current = window.setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else if (!isActive && time !== 0) {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    }
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [isActive, time]);
+
+  const handleStart = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
+  const formatTime = (time) => {
+    const getSeconds = `0${time % 60}`.slice(-2);
+    const minutes = Math.floor(time / 60);
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(time / 3600)}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="time-wrapper flex-center">
+      <div className="time-container flex-center glassmorphism">
+        <div className="time-div">
+          <div>
+            <h1 className="timer">{formatTime(time)}</h1>
+          </div>
+          <div className="button-container">
+            <button
+              className={`${!isActive ? 'button' : 'button alt'}`}
+              onClick={handleStart}
+            >
+              {!isActive ? 'Start' : 'Stop'}
+            </button>
+          </div>
+          <div>
+            <button className="button reset" onClick={handleReset}>
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
